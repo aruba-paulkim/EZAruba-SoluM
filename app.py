098123caca29ee9c2f41c5f8f4066c5e956b2b4a
Paul-kim <paul.kim@hpe.com>
2019-08-06 13:02:56 +0900

    make apicall function

--- app.py
+++ app.py
@@ -10,6 +10,27 @@ allow_device_id = {'C912B707-ED3D-490C-A154-B0C2A5E46189:com.arubanetworks.AppVi
                    'b312hb12':{'name':'정우성', 'company':'HPE Aruba', 'dept':'Meridian'},
                   }
 
+def apicall(url, display_id, display_str):
+    headers = {'content-type': 'application/json'}
+    url = 'http://192.168.99.212/inf/updateProductInfo'
+    payload = {'Header':{'StoreCode':'1038'},'Body':{'PRODUCT':[{'ITEM_ID':display_id,'ITEM_NAME':display_str,
+               'ALIAS':'','SALE_PRICE':1101,'SALE_PRICE_DECIMAL':'00','LIST_PRICE':0,'LIST_PRICE_DECIMAL':'00',
+               'UNIT_PRICE':0,'UNIT_PRICE_DECIMAL':'00','ORIGIN':'','MANUFACTURER':'','TYPE':'','WEIGHT':'','WEIGHT_UNIT':'',
+               'UNIT_PRICE_UNIT':'','UNIT_DIMENSION':'','A_MARKER':'','R_MARKER':'','CATEGORY1':'','CATEGORY2':'','CATEGORY3':'','CATEGORY4':'','CATEGORY5':'',
+               'DISPLAY_TYPE':'01','DISPLAY_TYPE2':'02','DISPLAY_TYPE3':'','NFC_URL':'','ETC_0':'','ETC_1':'','ETC_2':'','ETC_3':'','ETC_4':'','ETC_5':'','ETC_6':'','ETC_7':'','ETC_8':'','ETC_9':'','STORE_ID':'1038'}]}}
+
+    try:
+        r = requests.post(url, headers=headers, json=payload)
+        r.raise_for_status()
+        response = r.json()
+        return response
+    except Exception as e:
+        error_message = r.json()
+        print(error_message)
+        return error_message
+
+
+
 @app.route('/')
 def index():
     return 'Aruba Meridian - Solu-M API Middleware'
@@ -24,78 +45,36 @@ def meridian():
 
         #EZ-Work-Enter
         if "5679487262654464" == request.form['campaign_id'] :
-            headers = {'content-type': 'application/json'}
-            url = 'http://192.168.99.212/inf/updateProductInfo'
-            payload = {'Header':{'StoreCode':'1038'},'Body':{'PRODUCT':[{'ITEM_ID':'11','ITEM_NAME':info['company']+' '+info['name']+'님\n여기는 SmartWorkPlace 입니다.',
-                       'ALIAS':'','SALE_PRICE':1101,'SALE_PRICE_DECIMAL':'00','LIST_PRICE':0,'LIST_PRICE_DECIMAL':'00','UNIT_PRICE':0,'UNIT_PRICE_DECIMAL':'00','ORIGIN':'','MANUFACTURER':'',
-                       'TYPE':'','WEIGHT':'','WEIGHT_UNIT':'','UNIT_PRICE_UNIT':'','UNIT_DIMENSION':'','A_MARKER':'','R_MARKER':'','CATEGORY1':'','CATEGORY2':'','CATEGORY3':'','CATEGORY4':'','CATEGORY5':'',
-                       'DISPLAY_TYPE':'01','DISPLAY_TYPE2':'02','DISPLAY_TYPE3':'','NFC_URL':'','ETC_0':'','ETC_1':'','ETC_2':'','ETC_3':'','ETC_4':'','ETC_5':'','ETC_6':'','ETC_7':'','ETC_8':'','ETC_9':'','STORE_ID':'1038'}]}}
-            try:
-                r = requests.post(url, headers=headers, json=payload)
-                r.raise_for_status()
-                response = r.json()
-            except Exception as e:
-                error_message = r.json()
-                print(error_message)
-
+            display_id = 11
+            display_str = info['company']+' '+info['name']+'님\n여기는 SmartWorkPlace 입니다.'
+            response = apicall(display_id, display_str)
             res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n여기는 SmartWorkPlace 입니다.","path": ""}}'
             return res
 
 
         #EZ-Work-Exit
         elif "5355508601716736" == request.form['campaign_id'] :
-            headers = {'content-type': 'application/json'}
-            url = 'http://192.168.99.212/inf/updateProductInfo'
-            payload = {'Header':{'StoreCode':'1038'},'Body':{'PRODUCT':[{'ITEM_ID':'11','ITEM_NAME':'반갑습니다.\n여기는 SmartWorkPlace 입니다.',
-                       'ALIAS':'','SALE_PRICE':1101,'SALE_PRICE_DECIMAL':'00','LIST_PRICE':0,'LIST_PRICE_DECIMAL':'00','UNIT_PRICE':0,'UNIT_PRICE_DECIMAL':'00','ORIGIN':'','MANUFACTURER':'',
-                       'TYPE':'','WEIGHT':'','WEIGHT_UNIT':'','UNIT_PRICE_UNIT':'','UNIT_DIMENSION':'','A_MARKER':'','R_MARKER':'','CATEGORY1':'','CATEGORY2':'','CATEGORY3':'','CATEGORY4':'','CATEGORY5':'',
-                       'DISPLAY_TYPE':'01','DISPLAY_TYPE2':'02','DISPLAY_TYPE3':'','NFC_URL':'','ETC_0':'','ETC_1':'','ETC_2':'','ETC_3':'','ETC_4':'','ETC_5':'','ETC_6':'','ETC_7':'','ETC_8':'','ETC_9':'','STORE_ID':'1038'}]}}
-            try:
-                r = requests.post(url, headers=headers, json=payload)
-                r.raise_for_status()
-                response = r.json()
-            except Exception as e:
-                error_message = r.json()
-                print(error_message)
-
+            display_id = 11
+            display_str = '반갑습니다.\n여기는 SmartWorkPlace 입니다.'
+            response = apicall(display_id, display_str)
             res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n감사합니다..","path": ""}}'
             return res
 
 
         #EZ-Meeting-Enter
         elif "5555948702400512" == request.form['campaign_id'] :
-            headers = {'content-type': 'application/json'}
-            url = 'http://192.168.99.212/inf/updateProductInfo'
-            payload = {'Header':{'StoreCode':'1038'},'Body':{'PRODUCT':[{'ITEM_ID':'12','ITEM_NAME':info['company']+' '+info['name']+'님\n여기는 MeetingRoom 입니다.',
-                       'ALIAS':'','SALE_PRICE':1101,'SALE_PRICE_DECIMAL':'00','LIST_PRICE':0,'LIST_PRICE_DECIMAL':'00','UNIT_PRICE':0,'UNIT_PRICE_DECIMAL':'00','ORIGIN':'','MANUFACTURER':'',
-                       'TYPE':'','WEIGHT':'','WEIGHT_UNIT':'','UNIT_PRICE_UNIT':'','UNIT_DIMENSION':'','A_MARKER':'','R_MARKER':'','CATEGORY1':'','CATEGORY2':'','CATEGORY3':'','CATEGORY4':'','CATEGORY5':'',
-                       'DISPLAY_TYPE':'01','DISPLAY_TYPE2':'02','DISPLAY_TYPE3':'','NFC_URL':'','ETC_0':'','ETC_1':'','ETC_2':'','ETC_3':'','ETC_4':'','ETC_5':'','ETC_6':'','ETC_7':'','ETC_8':'','ETC_9':'','STORE_ID':'1038'}]}}
-            try:
-                r = requests.post(url, headers=headers, json=payload)
-                r.raise_for_status()
-                response = r.json()
-            except Exception as e:
-                error_message = r.json()
-                print(error_message)
+            display_id = 12
+            display_str = info['company']+' '+info['name']+'님\n여기는 MeetingRoom 입니다.'
+            response = apicall(display_id, display_str)
             res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n 여기는 MeetingRoom 입니다.","path": ""}}'
             return res 
 
 
         #EZ-Meeting-Exit
         elif "4908345245564928" == request.form['campaign_id'] :
-            headers = {'content-type': 'application/json'}
-            url = 'http://192.168.99.212/inf/updateProductInfo'
-            payload = {'Header':{'StoreCode':'1038'},'Body':{'PRODUCT':[{'ITEM_ID':'12','ITEM_NAME':'환영합니다.\n여기는 MeetingRoom 입니다.',
-                       'ALIAS':'','SALE_PRICE':1101,'SALE_PRICE_DECIMAL':'00','LIST_PRICE':0,'LIST_PRICE_DECIMAL':'00','UNIT_PRICE':0,'UNIT_PRICE_DECIMAL':'00','ORIGIN':'','MANUFACTURER':'',
-                       'TYPE':'','WEIGHT':'','WEIGHT_UNIT':'','UNIT_PRICE_UNIT':'','UNIT_DIMENSION':'','A_MARKER':'','R_MARKER':'','CATEGORY1':'','CATEGORY2':'','CATEGORY3':'','CATEGORY4':'','CATEGORY5':'',
-                       'DISPLAY_TYPE':'01','DISPLAY_TYPE2':'02','DISPLAY_TYPE3':'','NFC_URL':'','ETC_0':'','ETC_1':'','ETC_2':'','ETC_3':'','ETC_4':'','ETC_5':'','ETC_6':'','ETC_7':'','ETC_8':'','ETC_9':'','STORE_ID':'1038'}]}}
-            try:
-                r = requests.post(url, headers=headers, json=payload)
-                r.raise_for_status()
-                response = r.json()
-            except Exception as e:
-                error_message = r.json()
-                print(error_message)
+            display_id = 12
+            display_str = '환영합니다\n여기는 MeetingRoom 입니다.'
+            response = apicall(display_id, display_str)
             res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n감사합니다.","path": ""}}'
             return res 
 
