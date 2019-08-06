8d6f550fdf9a76015d935dbd076f25a05b5cdfd5
Paul-kim <paul.kim@hpe.com>
2019-08-06 14:31:52 +0900

    update

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import requests
 
 app = Flask(__name__)
 
-allow_device_id = {'C912B707-ED3D-490C-A154-B0C2A5E46189:com.arubanetworks.AppViewer':{'name':'김민혁', 'company':'HPE Aruba', 'dept':'Korea SE'},
-                   'C912B707-ED3D-490C-A154-B0C2A5E46189:com.arubanetworks.aruba-meridian':{'name':'김민혁', 'company':'HPE Aruba', 'dept':'Korea SE'},
-                   '769c0ef4-06d0-4ff4-bfae-a5e104b88f1c':{'name':'조용운', 'company':'HPE Aruba', 'dept':'Korea SE'},
-                   'd9b9f9a4-1cf5-4871-8945-b749e50d95d7':{'name':'갤럭시텝A', 'company':'HPE Aruba', 'dept':'Korea SE'},
-                   'b312hb12':{'name':'정우성', 'company':'HPE Aruba', 'dept':'Meridian'},
+allow_device_id = {
+                   'C912B707-ED3D-490C-A154-B0C2A5E46189':{'name':'김민혁', 'company':'HPE Aruba', 'dept':'Korea SE'},
+                   '769C0EF4-06D0-4FF4-BFAE-A5E104B88F1C':{'name':'조용운', 'company':'HPE Aruba', 'dept':'Korea SE'},
+                   'D9B9F9A4-1CF5-4871-8945-B749E50D95D7':{'name':'갤럭시텝A', 'company':'HPE Aruba', 'dept':'Korea SE'},
+                   'B312HB12':{'name':'정우성', 'company':'HPE Aruba', 'dept':'Meridian'},
                   }
 
-def apicall(url, display_id, display_str):
+def apicall(display_id, display_str):
     headers = {'content-type': 'application/json'}
     url = 'http://192.168.99.212/inf/updateProductInfo'
     payload = {'Header':{'StoreCode':'1038'},'Body':{'PRODUCT':[{'ITEM_ID':display_id,'ITEM_NAME':display_str,
@@ -18,7 +18,6 @@ def apicall(url, display_id, display_str):
                'UNIT_PRICE':0,'UNIT_PRICE_DECIMAL':'00','ORIGIN':'','MANUFACTURER':'','TYPE':'','WEIGHT':'','WEIGHT_UNIT':'',
                'UNIT_PRICE_UNIT':'','UNIT_DIMENSION':'','A_MARKER':'','R_MARKER':'','CATEGORY1':'','CATEGORY2':'','CATEGORY3':'','CATEGORY4':'','CATEGORY5':'',
                'DISPLAY_TYPE':'01','DISPLAY_TYPE2':'02','DISPLAY_TYPE3':'','NFC_URL':'','ETC_0':'','ETC_1':'','ETC_2':'','ETC_3':'','ETC_4':'','ETC_5':'','ETC_6':'','ETC_7':'','ETC_8':'','ETC_9':'','STORE_ID':'1038'}]}}
-
     try:
         r = requests.post(url, headers=headers, json=payload)
         r.raise_for_status()
@@ -37,56 +36,45 @@ def index():
 
 @app.route('/api/meridian', methods=['GET', 'POST'])
 def meridian():
-
     # device_id가 allow_device_id 에 있는지 확인
-    device_id = request.form['device_id']
-    if device_id in allow_device_id :
-        info = allow_device_id[device_id]
-
-        #EZ-Work-Enter
-        if "5679487262654464" == request.form['campaign_id'] :
-            display_id = 11
-            display_str = info['company']+' '+info['name']+'님\n여기는 SmartWorkPlace 입니다.'
-            response = apicall(display_id, display_str)
-            res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n여기는 SmartWorkPlace 입니다.","path": ""}}'
-            return res
-
-
-        #EZ-Work-Exit
-        elif "5355508601716736" == request.form['campaign_id'] :
-            display_id = 11
-            display_str = '반갑습니다.\n여기는 SmartWorkPlace 입니다.'
-            response = apicall(display_id, display_str)
-            res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n감사합니다..","path": ""}}'
-            return res
-
-
-        #EZ-Meeting-Enter
-        elif "5555948702400512" == request.form['campaign_id'] :
-            display_id = 12
-            display_str = info['company']+' '+info['name']+'님\n여기는 MeetingRoom 입니다.'
-            response = apicall(display_id, display_str)
-            res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n 여기는 MeetingRoom 입니다.","path": ""}}'
-            return res 
-
-
-        #EZ-Meeting-Exit
-        elif "4908345245564928" == request.form['campaign_id'] :
-            display_id = 12
-            display_str = '환영합니다\n여기는 MeetingRoom 입니다.'
-            response = apicall(display_id, display_str)
-            res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n감사합니다.","path": ""}}'
-            return res 
-
-
-        else :
-            # unregisted campaign_id
-            return '{}'
-
-    else :
+    req_device_id = request.form['device_id'][0:36].upper()
+    try:
+        info = allow_device_id[req_device_id]
+        print(info)
+    except Exception as e:
+        #print(e)
         print("unknown device_id : "+request.form['device_id'])
         return '{}'
 
+    #EZ-Work-Enter
+    if "5679487262654464" == request.form['campaign_id'] :
+        display_id = 11
+        display_str = info['company']+' '+info['name']+'님\n여기는 SmartWorkPlace 입니다.'
+        response = apicall(display_id, display_str)
+        res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n여기는 SmartWorkPlace 입니다.","path": ""}}'
+
+    #EZ-Work-Exit
+    elif "5355508601716736" == request.form['campaign_id'] :
+        display_id = 11
+        display_str = '반갑습니다.\n여기는 SmartWorkPlace 입니다.'
+        response = apicall(display_id, display_str)
+        res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n안녕히가세요.","path": ""}}'
+
+    #EZ-Meeting-Enter
+    elif "5555948702400512" == request.form['campaign_id'] :
+        display_id = 12
+        display_str = info['company']+' '+info['name']+'님\n여기는 MeetingRoom 입니다.'
+        response = apicall(display_id, display_str)
+        res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n여기는 MeetingRoom 입니다.","path": ""}}'
+
+    #EZ-Meeting-Exit
+    elif "4908345245564928" == request.form['campaign_id'] :
+        display_id = 12
+        display_str = '환영합니다\n여기는 MeetingRoom 입니다.'
+        response = apicall(display_id, display_str)
+        res = '{"notification":{"title": "EZ Aruba","message": "'+info['company']+' '+info['name']+'님\\n안녕히가세요","path": ""}}'
+
+    return res
 
 
 if __name__ == '__main__':
