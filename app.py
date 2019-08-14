f41d56a367b32b63452623224a504e7738f9857f
Paul-kim <paul.kim@hpe.com>
2019-08-14 13:27:40 +0900

    add admin function

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request, json, render_template
-import requests
+from flask import Flask, request, json, render_template, session, redirect
+import requests, sqlite3
 
 app = Flask(__name__)
+app.secret_key = b'aruba1234567890'
 
-with open('allow_device_id.txt', encoding='utf-8-sig') as data_file:
-    allow_device_id = json.load(data_file)
 with open('inform_str.txt', encoding='utf-8-sig') as data_file:
     inform_str = json.load(data_file)
 
@@ -29,10 +28,84 @@ def apicall(display_id, display_str):
         return error_message
 
 
-
 @app.route('/')
 def index():
-    return inform_str['system_name']
+    if 'sess_admin' in session :
+        return redirect('/setup')
+    else :
+        return redirect('/login')
+        #return 'ERROR'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'GET' :
+        if 'sess_admin' in session :
+            return redirect('/setup')
+        else :
+            return render_template("login.html")
+
+    elif request.method == 'POST' :        
+        con = sqlite3.connect('database.db')
+        cur = con.cursor()
+        cur.execute("select * from admin where admin_id=? and admin_pw=?", 
+            (request.form['admin_id'], request.form['admin_pw']))
+        rows = cur.fetchall()
+
+        if len(rows) > 0 : 
+            session['sess_admin'] = request.form['admin_id']
+        else :
+            print('login fail')
+
+        con.close()
+        return redirect('/')
+
+    else :
+        return 'ERROR'
+
+
+@app.route('/logout')
+def logout():
+    session.pop('sess_admin', None)
+    return redirect('/')
+
+
+@app.route('/setup', methods=['GET', 'POST'])
+def setup():
+    if 'sess_admin' in session :
+        if request.method == 'GET' :
+            con = sqlite3.connect('database.db')
+            cur = con.cursor()
+            cur.execute("select * from allowed_device")
+            rows = cur.fetchall()
+            con.close()
+            return render_template("setup.html", rows=rows)
+        elif request.method == 'POST' :
+            selected = request.form.getlist('demo_user')
+            print(selected)
+            con = sqlite3.connect('database.db')
+            cur = con.cursor()
+            cur.execute("select * from allowed_device")
+            rows = cur.fetchall()
+            enable_YN = ""
+            for row in rows :
+                if str(row[0]) in selected :
+                    enable_YN = "Y"
+                else :
+                    enable_YN = "N"
+
+                print(str(row[0])+" is "+enable_YN)
+                cur.execute("update allowed_device set enable_YN=? where seq =?", (enable_YN, row[0]))
+                con.commit()
+
+            con.close()
+            return redirect('/setup')
+        else :
+            return 'ERROR'
+    else :
+        return redirect('/')
+
 
 @app.route('/show/<location>', methods=['GET', 'POST'])
 def show(location):
@@ -49,40 +122,45 @@ def show(location):
     else :
         return ''
 
+
 @app.route('/api/meridian', methods=['GET', 'POST'])
 def meridian():
     # device_id가 allow_device_id 에 있는지 확인
     req_device_id = request.form['device_id'][0:36].upper()
-    try:
-        info = allow_device_id[req_device_id]
-    except Exception as e:
-        #print(e)
-        print("unknown device_id : "+request.form['device_id'])
+
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    cur.execute("select * from allowed_device where enable_YN ='Y' and device_id=?", req_device_id)
+    rows = cur.fetchall()
+    if len(rows) > 0 : 
+        info = rows[0]
+    else :
+        print("unknown or not enabled device_id : "+request.form['device_id'])
         return '{}'
 
     #EZ-Work-Enter
     if "5679487262654464" == request.form['campaign_id'] :
         display_id = '11'
-        display_str = inform_str['solum_work_enter'].replace('##name##',info['name'])
+        display_str = inform_str['solum_work_enter'].replace('##name##',info['user_name'])
         response = apicall(display_id, display_str)
-        meridian_str = inform_str['meridian_work_enter'].replace('##name##',info['name'])
-        print("EZ-Work-Enter / " + info['name'] + " / "+response['Header']['ResultCode'])
+        meridian_str = inform_str['meridian_work_enter'].replace('##name##',info['user_name'])
+        print("EZ-Work-Enter / " + info['user_name'] + " / "+response['Header']['ResultCode'])
 
     #EZ-Work-Exit
     elif "5355508601716736" == request.form['campaign_id'] :
         display_id = '11'
         display_str = inform_str['solum_work_exit']
         response = apicall(display_id, display_str)
-        meridian_str = inform_str['meridian_work_exit'].replace('##name##',info['name'])
-        print("EZ-Work-Exit / " + info['name'] + " / "+response['Header']['ResultCode'])
+        meridian_str = inform_str['meridian_work_exit'].replace('##name##',info['user_name'])
+        print("EZ-Work-Exit / " + info['user_name'] + " / "+response['Header']['ResultCode'])
 
     #EZ-Meeting-Enter
     elif "5555948702400512" == request.form['campaign_id'] :
         display_id = '12'
-        display_str = inform_str['solum_meeting_enter'].replace('##name##',info['name'])
+        display_str = inform_str['solum_meeting_enter'].replace('##name##',info['user_name'])
         response = apicall(display_id, display_str)
-        meridian_str = inform_str['meridian_meeting_enter'].replace('##name##',info['name'])
-        print("EZ-Meeting-Enter / " + info['name'] + " / "+response['Header']['ResultCode'])
+        meridian_str = inform_str['meridian_meeting_enter'].replace('##name##',info['user_name'])
+        print("EZ-Meeting-Enter / " + info['user_name'] + " / "+response['Header']['ResultCode'])
         fd = open("static/show-meetingroom.txt", "w")
         fd.write(display_str.replace("\n","<br>"))
         fd.close()
@@ -92,8 +170,8 @@ def meridian():
         display_id = '12'
         display_str = inform_str['solum_meeting_exit']
         response = apicall(display_id, display_str)
-        meridian_str = inform_str['meridian_meeting_exit'].replace('##name##',info['name'])
-        print("EZ-Meeting-Exit / " + info['name'] + " / "+response['Header']['ResultCode'])
+        meridian_str = inform_str['meridian_meeting_exit'].replace('##name##',info['user_name'])
+        print("EZ-Meeting-Exit / " + info['user_name'] + " / "+response['Header']['ResultCode'])
         fd = open("static/show-meetingroom.txt", "w")
         fd.write(display_str.replace("\n","<br>"))
         fd.close()
@@ -106,4 +184,5 @@ def meridian():
 
 
 if __name__ == '__main__':
+    
     app.run(host='0.0.0.0', port=8080, debug=True)
